06fa8ddba446c48b827e02af158bdaf1417bacc1
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-17 02:17:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <div className='flex flex-col min-h-screen w-screen'>
+      <header>HEADER</header>
+      <main className='flex-1'></main>
+      <footer>FOOTER</footer>
+    </div>
+  )
+}
+
+export default App
