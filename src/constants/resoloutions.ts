8d55d1699094cc5861bfaafe133af9ef785d6c6e
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-22 08:44:51 -0300

    added resulutions

--- src/constants/resoloutions.ts
+++ src/constants/resoloutions.ts
@@ -4,7 +4,10 @@ const RESOLUTIONS: Dimension[] = [
   { width: 306, height: 306 },
   { width: 323, height: 323 },
   { width: 340, height: 340 },
-  { width: 357, height: 357 }
+  { width: 357, height: 357 },
+  { width: 374, height: 374 },
+  { width: 391, height: 391 },
+  { width: 408, height: 408 }
 ]
 
 export default RESOLUTIONS
