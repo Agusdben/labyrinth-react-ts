92916e08c6681d4682cb75be4aebe2b19c9452e1
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-22 11:44:35 -0300

    added blocked levels

--- src/pages/Levels/index.tsx
+++ src/pages/Levels/index.tsx
@@ -5,6 +5,7 @@ import PageSection from '../../components/PageSection'
 import Title from '../../components/Title'
 import MenuButton from '../../components/MenuButtons'
 import { useState } from 'react'
+import { AiFillLock } from 'react-icons/ai'
 
 const Levels = () => {
   const [levelSelected, setLevelSelected] = useState<number>(-1)
@@ -20,22 +21,31 @@ const Levels = () => {
       <Title>Levels</Title>
       <WindowContainer>
         <ul className='flex-1 grid grid-cols-3 gap-4 overflow-y-auto px-2'>
-          {LEVELS.map((l, index) => (
-            <li className='border grid p-2 aspect-square' key={l.level}>
-              <button
-                className={`${
-                  index + 1 === levelSelected
-                    ? 'bg-zinc-200 text-zinc-900'
-                    : 'bg-zinc-900 text-zinc-200' // plus one because first level is 1
-                } `}
-                onClick={handleLevelSelected(l.level)}
-              >
-                <p>
-                  level <span>{l.level}</span>
-                </p>
-              </button>
-            </li>
-          ))}
+          {LEVELS.map(l => {
+            const isBlocked = l.level > 2
+            const isLevelSelected = l.level === levelSelected
+            return (
+              <li className='border grid p-2 aspect-square' key={l.level}>
+                <button
+                  className={`${
+                    isLevelSelected ? 'bg-zinc-200 text-zinc-900' : ''
+                  } `}
+                  disabled={isBlocked}
+                  onClick={
+                    !isBlocked ? handleLevelSelected(l.level) : undefined
+                  }
+                >
+                  {isBlocked ? (
+                    <AiFillLock className='w-full' />
+                  ) : (
+                    <p>
+                      level <span>{l.level}</span>
+                    </p>
+                  )}
+                </button>
+              </li>
+            )
+          })}
         </ul>
         <MenuButton
           disabled={levelSelected < 1}
