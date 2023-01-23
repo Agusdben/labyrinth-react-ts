03958b2023cf8d7e34d1b982b2b3cc748f5b9d7a
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-23 03:55:10 -0300

    removed tutorial folder

--- src/pages/HowToPlay/index.tsx
+++ src/pages/HowToPlay/index.tsx
@@ -1,10 +1,6 @@
-import ImageSlider from '../../components/ImageSlider'
 import PageSection from '../../components/PageSection'
 import Title from '../../components/Title'
 import WindowContainer from '../../components/WindowContainer'
-import fer from '../../assets/tutorial/fer.png'
-import ferRuedas from '../../assets/tutorial/Fernando_envalido.png'
-const images = [fer, ferRuedas]
 
 const HowToPlay = () => {
   return (
@@ -17,7 +13,6 @@ const HowToPlay = () => {
           title='How to play my game'
           allowFullScreen
         />
-        {/* <ImageSlider images={images} /> */}
       </WindowContainer>
     </PageSection>
   )
