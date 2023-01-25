c8900af9305f52b56ff8e08fd4737d92a3b43d1e
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-25 08:01:32 -0300

    Added resultionsAvalible

--- src/pages/OptionsGraphics/Components/ResolutionSelect/index.tsx
+++ src/pages/OptionsGraphics/Components/ResolutionSelect/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RESOLUTIONS from '../../../../constants/resoloutions'
 import useOptions from '../../../../hooks/useOptions'
 import { Dimension } from '../../../../types'
 
 const ResolutionSelect = () => {
   const { setResolution, resolution } = useOptions()
+  const [resolutionsAvalible, setResolutionsAvalible] = useState<
+    Array<Dimension>
+  >([])
+
   const handleResolution = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target
     const resArr: Array<number> = value.split('x').map(val => Number(val))
     const res: Dimension = { width: resArr[0], height: resArr[1] }
     setResolution(res)
   }
+
+  useEffect(() => {
+    const offset = 10
+    const diviceWindth = window.innerWidth - offset
+    setResolutionsAvalible(RESOLUTIONS.filter(res => res.width < diviceWindth))
+  }, [])
+
   return (
     <div className='flex gap-10 justify-center items-center'>
       <p>Resolution:</p>
@@ -19,7 +30,7 @@ const ResolutionSelect = () => {
         className='bg-transparent border-2 border-orange-300 px-6 py-2 hover:cursor-pointer focus:outline-none'
         onChange={e => handleResolution(e)}
       >
-        {RESOLUTIONS.map(res => {
+        {resolutionsAvalible.map(res => {
           const resolution: string = `${res.width}x${res.height}`
           return (
             <option
